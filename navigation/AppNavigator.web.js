ee170ca9ff914d4684588042b375d96e3a8d1e54
sairim <spiri@protonmail.com>
2019-09-13 16:34:09 +0300

    bring AppNavigator for web up to date

--- navigation/AppNavigator.web.js
+++ navigation/AppNavigator.web.js
@@ -1,13 +1,25 @@
-import { createBrowserApp } from '@react-navigation/web';
-import { createStackNavigator } from 'react-navigation';
+import { createBrowserApp } from "@react-navigation/web";
+import { createStackNavigator } from "react-navigation";
 
-import HomeScreen from '../screens/HomeScreen';
-import ListTabNavigator from './ListTabNavigator';
+import HomeScreen from "../screens/HomeScreen";
+import ListTabNavigator from "./ListTabNavigator";
 
-const switchNavigator = createStackNavigator({
-  Home: HomeScreen,
-  List: ListTabNavigator,
-});
-switchNavigator.path = '';
+const stackNavigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    List: ListTabNavigator
+  },
+  {
+    initialRouteName: "Home",
+    defaultNavigationOptions: {
+      headerTitleStyle: {
+        fontFamily: "Pacifico",
+        color: "rgba(37, 204, 247, 1)",
+        fontSize: 22
+      }
+    }
+  }
+);
+stackNavigator.path = "";
 
-export default createBrowserApp(switchNavigator, { history: 'hash' });
+export default createBrowserApp(stackNavigator, { history: "hash" });
